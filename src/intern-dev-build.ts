5f2b51c0d58b108b81d77ebbafd64efccc345148
Jason Cheatham <j.cheatham@gmail.com>
2016-12-15 14:55:58 -0500

    Change build order
    - Copy resources before building ts code
    - Use getConfigs to build root project first

--- src/intern-dev-build.ts
+++ src/intern-dev-build.ts
@@ -2,17 +2,17 @@
 
 import { echo } from 'shelljs';
 import { join, dirname } from 'path';
-import { buildDir, copyAll, exec, getResources, glob } from './common';
+import { buildDir, copyAll, exec, getConfigs, getResources } from './common';
 
-glob('**/tsconfig.json').forEach(function (tsconfig) {
-	echo(`## Compiling ${dirname(tsconfig)}`);
-	exec(`tsc -p "${tsconfig}"`);
-});
-
+// Copy resources first in case some of them are needed for builds
 const resources = getResources();
-
 Object.keys(resources).forEach(function (dest) {
 	copyAll(resources[dest], join(buildDir, dest));
+});
+
+getConfigs().forEach(function (tsconfig) {
+	echo(`## Compiling ${dirname(tsconfig)}`);
+	exec(`tsc -p "${tsconfig}"`);
 });
 
 echo('## Done building');
